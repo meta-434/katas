8e2790886f110d5777e409ddc33226d914b60aca
Alex Hapgood <ach1@vt.edu>
2023-08-16 21:23:00 -0400

    IN PROGRESS sumOfPairs.ts solution
    still need to implement return solution pair with higher 2nd value index when multiple solution pairs exist.

---
+++ sumOfPairs.ts
@@ -0,0 +1,69 @@
+// Sum of Pairs
+// Given a list of integers and a single sum value,
+// return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+// If there are two or more pairs with the required sum, the pair whose second element
+// has the smallest index is the solution.
+
+export function sumPairs(ints: number[], s: number): [number, number] | void {
+    let soln: [number, number] = [undefined, undefined];
+    let done = false;
+    ints.forEach((integer: number, idx: number) => {
+        if (done) {
+            return;
+        } else {
+            for (let i: number = idx + 1; i < ints.length; i++) {
+                //console.log(`at number in position ${i} of array, value ${ints[i]}`);
+                if (integer + ints[i] === s) {
+                    console.log(`solution! ${integer} + ${ints[i]} === ${s}`);
+                    soln[0] = (integer);
+                    soln[1] = (ints[i]);
+                    done = true;
+                    break;
+                }
+            }
+        }
+
+    });
+    console.log('soln equals ', soln);
+    if (soln[0] === undefined && soln[1] === undefined) return undefined;
+    else return soln;
+}
+
+
+import {assert} from "chai";
+
+const l1: number[] = [1, 4, 8, 7, 3, 15],
+    l2: number[] = [1, -2, 3, 0, -6, 1],
+    l3: number[] = [20, -13, 40],
+    l4: number[] = [1, 2, 3, 4, 1, 0],
+    l5: number[] = [10, 5, 2, 3, 7, 5],
+    l6: number[] = [4, -2, 3, 3, 4],
+    l7: number[] = [0, 2, 0],
+    l8: number[] = [5, 9, 13, -3];
+
+describe("example", function () {
+    it("test", () => {
+        assert.deepEqual(sumPairs(l1, 8), [1, 7], "Basic: [" + l1 + "] should return [1, 7] for sum = 8");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l2, -6), [0, -6], "Negatives: [" + l2 + "] should return [0, -6] for sum = -6");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l3, -7), undefined, "No Match: [" + l3 + "] should return undefined for sum = -7");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l4, 2), [1, 1], "First Match From Left: [" + l4 + "] should return [1, 1] for sum = 2 ");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l5, 10), [3, 7], "First Match From Left REDUX!: [" + l5 + "] should return [3, 7] for sum = 10 ");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l6, 8), [4, 4], "Duplicates: [" + l6 + "] should return [4, 4] for sum = 8");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l7, 0), [0, 0], "Zeroes: [" + l7 + "] should return [0, 0] for sum = 0");
+    });
+    it("test", () => {
+        assert.deepEqual(sumPairs(l8, 10), [13, -3], "Subtraction: [" + l8 + "] should return [13, -3] for sum = 10");
+    });
+});
